ca4a180457e6275d235646fdcb0bfdb92183ae24
toto <toto.zhang@gmail.com>
2019-03-27 23:59:44 +0800

    matplot added, interactive mode.

--- game/main.py
+++ game/main.py
@@ -4,6 +4,7 @@ from game import proc, draw
 from game.agent import Agent
 from game.setting import Setting
 from game.sugarscape import SugarScape
+from matplotlib import pyplot
 
 
 def main():
@@ -28,6 +29,10 @@ def main():
     draw.drawAgents(screen, agents)
     pygame.display.flip()
 
+    pyplot.ion()
+    fig = pyplot.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
     while True:
 
         for ev in pygame.event.get():
@@ -48,6 +53,18 @@ def main():
             fpsclock.tick(setting.FPS)
             setting.Round += 1
             pygame.display.flip()
+
+            #pyplot
+            wealths = []
+            for agent in agents:
+                wealths.append(agent.energy)
+            ax.hist(wealths, 8)
+            pyplot.xlabel('Interval')
+            pyplot.ylabel('Population')
+            pyplot.title('Wealth distribution')
+            pyplot.grid(True)
+            pyplot.pause(0.03)
+            pyplot.cla()
 
 
 if __name__ == "__main__":
